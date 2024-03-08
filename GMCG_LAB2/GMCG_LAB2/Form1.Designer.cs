a91744cfc3c4871f2d604e353f9140c0b94ea1fc
MypkaXD <nicklayschannel@yandex.ru>
2024-03-08 13:27:45 +0300

    added func for add points

--- GMCG_LAB2/GMCG_LAB2/Form1.Designer.cs
+++ GMCG_LAB2/GMCG_LAB2/Form1.Designer.cs
@@ -31,11 +31,14 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox_2d = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_2d)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(412, 134);
+            this.button1.Location = new System.Drawing.Point(562, 94);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -45,7 +48,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(461, 301);
+            this.textBox1.Location = new System.Drawing.Point(636, 165);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
@@ -53,22 +56,46 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(461, 348);
+            this.textBox2.Location = new System.Drawing.Point(636, 212);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 2;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label1.Location = new System.Drawing.Point(24, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(321, 39);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Координаты точек:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox_2d
+            // 
+            this.pictureBox_2d.Location = new System.Drawing.Point(822, 94);
+            this.pictureBox_2d.Name = "pictureBox_2d";
+            this.pictureBox_2d.Size = new System.Drawing.Size(814, 480);
+            this.pictureBox_2d.TabIndex = 4;
+            this.pictureBox_2d.TabStop = false;
+            this.pictureBox_2d.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1734, 672);
+            this.Controls.Add(this.pictureBox_2d);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_2d)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,6 +106,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox_2d;
     }
 }
 
--- GMCG_LAB2/GMCG_LAB2/Form1.cs
+++ GMCG_LAB2/GMCG_LAB2/Form1.cs
@@ -15,38 +15,105 @@ namespace GMCG_LAB2
     public partial class Form1 : Form
     {
 
+        List<Point> MyPolygon = new List<Point>();
+        Pen pen1;
+        int x0, x, y0, y, n = 0;
+        Bitmap canvas;
+        Color user_color;
+        Graphics user_Graphics;
+        Font drawFont = new Font("Arial", 10);
+        Font axFont = new Font("Tahoma", 10);
+        SolidBrush drawBrush = new SolidBrush(Color.Black);
+        SolidBrush axBrush = new SolidBrush(Color.Gray);
+        // формат строки
+        StringFormat drawFormat = new StringFormat();
+
         private class Coords
         {
             public int x { get; set; }
             public int y { get; set; }
         }
 
-        int n = 0;
+        int j = 0;
 
         private List<Coords> coords = new List<Coords>();
 
         public Form1()
         {
             InitializeComponent();
+            user_color = Color.Blue;
+            pen1 = new Pen(user_color, 2);
+            canvas = new Bitmap(pictureBox_2d.Width,
+            pictureBox_2d.Height);
+            user_Graphics = Graphics.FromImage(canvas);
+            x0 = (int)(pictureBox_2d.Width / 2);
+            y0 = (int)(pictureBox_2d.Height / 2);
+            user_Graphics.FillRectangle(Brushes.White, 0, 0,
+            pictureBox_2d.Width, pictureBox_2d.Height);
+            user_Graphics.DrawLine(Pens.Gray, x0, 2, x0,
+            pictureBox_2d.Height - 2);
+            user_Graphics.DrawLine(Pens.Gray, 2, y0,
+            pictureBox_2d.Width - 2, y0);
+            pictureBox_2d.Image = canvas;
+
+            user_Graphics.DrawString("x", axFont, axBrush, (float)(pictureBox_2d.Width - 10), (float)(y0 + 3), drawFormat);
+            user_Graphics.DrawString("y", axFont, axBrush, (float)(x0 - 10), (float)(3.0), drawFormat);
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int x, y;
+            int temp_x = 0;
+            int temp_y = 0;
 
-            if (Int32.TryParse(this.textBox1.Text, out x) && 
-                Int32.TryParse(this.textBox2.Text, out y))
+            if (Int32.TryParse(this.textBox1.Text, out temp_x) && 
+                Int32.TryParse(this.textBox2.Text, out temp_y))
             {
-                ++n;
+                ++j;
                 Coords coord = new Coords();
-                coord.x = x;
-                coord.y = y;
+                coord.x = temp_x;
+                coord.y = temp_y;
                 coords.Add(coord);
             }
             else
             {
                 MessageBox.Show("Ввод данных произведен неверно!", "Ошибка ввода", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+
+            this.label1.Text = "Координаты точек:\n";
+
+            foreach (var coord in coords)
+            {
+                this.label1.Text += "(" + System.Convert.ToString(coord.x) + "; " + System.Convert.ToString(coord.y) + ")\n";
+            }
+
+            if (MyPolygon == null) 
+                return;
+            MyPolygon.Add(new Point(temp_x, temp_y));
+            user_Graphics.FillRectangle(Brushes.White, 0, 0, pictureBox_2d.Width, pictureBox_2d.Height);
+            if (MyPolygon.Count < 3)
+            {
+                foreach (var point in MyPolygon)
+                    user_Graphics.FillEllipse(Brushes.Blue, point.X - 2, point.Y - 2, 4, 4);
+            }
+            else
+                user_Graphics.DrawPolygon(pen1, MyPolygon.ToArray());
+            pictureBox_2d.Image = canvas;
+
+            foreach (var point in MyPolygon)
+                user_Graphics.DrawString(string.Format("p{0:d}",
+                MyPolygon.IndexOf(point)), drawFont, drawBrush,
+                (float)(point.X + 3), (float)(point.Y + 3), drawFormat);
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
@@ -55,6 +122,11 @@ namespace GMCG_LAB2
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
