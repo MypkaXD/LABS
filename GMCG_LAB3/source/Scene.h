719b5dd6fb944b8b48312137e25c5d9dc26684d5
MypkaXD <nicklayschannel@yandex.ru>
2024-05-20 21:01:49 +0300

    delete trash

---
+++ GMCG_LAB3/source/Scene.h
@@ -0,0 +1,278 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include <glut.h>
+#include <vector>
+
+#include "Types.h"
+#include "Balls.h"
+#include "Game.h"
+
+#pragma once
+
+class Scene {
+
+private:
+
+	float m_F_deltaTime = 0.001f;
+
+	Balls m_Balls_balls;
+
+	float m_F_x_border = 4;
+	float m_F_y_border = 4;
+	float m_F_z_border = 4;
+
+public:
+
+	void draw_floor() {
+		
+		Point3D vertices[] = {
+		{1,1,0},
+		{1,-1,0},
+		{-1,-1,0},
+		{-1,1,0}
+		};
+
+		glEnableClientState(GL_VERTEX_ARRAY);
+
+		glVertexPointer(3, GL_FLOAT, 0, &vertices);
+
+		for (int i = 0; i < 5; ++i) {
+			for (int j = 0; j < 5; ++j) {
+
+				glPushMatrix();
+
+				if ((i + j) % 2 == 0)
+					glColor3f(0.5, 1, 0);
+				else
+					glColor3f(1, 1, 1);
+
+				glTranslatef(i * 2, j * 2, 0);
+				glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+
+				glPopMatrix();
+
+			}
+		}
+
+		glDisableClientState(GL_VERTEX_ARRAY);
+	
+	}
+
+	void update(const std::vector<GLuint> textures) {
+
+		glPushMatrix();
+
+		glScalef(2, 2, 2);
+
+		createLight();
+
+		m_Balls_balls.updateBalls(m_F_deltaTime, textures[1], m_F_x_border,m_F_y_border, m_F_z_border);
+
+		draw_box(textures[2]);
+		
+		glPopMatrix();
+	}
+
+	void createLight() {
+
+		glEnable(GL_LIGHTING);
+		glEnable(GL_LIGHT0);
+
+		GLfloat material_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
+
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, material_diffuse);
+
+		// Настройка освещения
+		GLfloat light_diffuse[] = { 1, 1, 1 };
+		GLfloat light_position[] = { 1.0, -1, -1, 0.0 };
+		glEnable(GL_LIGHT0);
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+		glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+		GLfloat light1_diffuse[] = { 1,0, 0};
+		GLfloat light1_position[] = { 1, 1, 1.0, 1.0 };
+		glEnable(GL_LIGHT1);
+		glLightfv(GL_LIGHT1, GL_DIFFUSE, light1_diffuse);
+		glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
+
+		GLfloat light2_diffuse[] = { 0,1, 0 };
+		GLfloat light2_position[] = { -1, -1, 1.0, 1.0 };
+		glEnable(GL_LIGHT2);
+		glLightfv(GL_LIGHT2, GL_DIFFUSE, light2_diffuse);
+		glLightfv(GL_LIGHT2, GL_POSITION, light2_position);
+	}
+
+	void draw_box(GLuint texture) {
+
+		glEnable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_ONE, GL_ONE);
+
+		float ambient[4] = { 0.2, 0.2, 0.2,1.0 };
+		glMaterialfv(GL_FRONT, GL_AMBIENT, ambient);
+
+		// Начало рисования параллелепипеда
+		glBegin(GL_QUADS);
+
+		glColor4f(1, 1, 1, 0.2f);
+		// Передняя грань (z = 1.0)
+		//glColor3f(1, 0.0, 0.0); // Красный цвет
+		glTexCoord2f(0.0f, 0.0f);	glVertex3f(-m_F_x_border, -m_F_y_border, m_F_z_border);
+		glTexCoord2f(1.0f, 0.0f);	glVertex3f(m_F_x_border, -m_F_y_border, m_F_z_border);
+		glTexCoord2f(1.0f, 1.0f);	glVertex3f(m_F_x_border, m_F_y_border, m_F_z_border);
+		glTexCoord2f(0.0f, 1.0f);	glVertex3f(-m_F_x_border, m_F_y_border, m_F_z_border);
+
+		// Задняя грань (z = -1.0)
+		//glColor3f(0.0, 1, 0.0); // Зеленый цвет
+		glTexCoord2f(1.0f, 0.0f);	glVertex3f(-m_F_x_border, -m_F_y_border, -m_F_z_border);
+		glTexCoord2f(1.0f, 1.0f);	glVertex3f(m_F_x_border, -m_F_y_border, -m_F_z_border);
+		glTexCoord2f(0.0f, 1.0f);	glVertex3f(m_F_x_border, m_F_y_border, -m_F_z_border);
+		glTexCoord2f(0.0f, 0.0f);	glVertex3f(-m_F_x_border, m_F_y_border, -m_F_z_border);
+
+		// Левая грань (x = -1.0)
+		//glColor3f(0.0, 0.0, 1); // Синий цвет
+		//glTexCoord2f(1.0f, 0.0f);	glVertex3f(-m_F_x_border, -m_F_y_border, -m_F_z_border);
+		//glTexCoord2f(1.0f, 1.0f);	glVertex3f(-m_F_x_border, -m_F_y_border, m_F_z_border);
+		//glTexCoord2f(0.0f, 1.0f);	glVertex3f(-m_F_x_border, m_F_y_border, m_F_z_border);
+		//glTexCoord2f(0.0f, 0.0f);	glVertex3f(-m_F_x_border, m_F_y_border, -m_F_z_border);
+
+		// Правая грань (x = 1.0)
+		//glColor3f(1.0, 1.0, 0.0); // Желтый цвет
+		glTexCoord2f(0.0f, 0.0f);	glVertex3f(m_F_x_border, -m_F_y_border, -m_F_z_border);
+		glTexCoord2f(1.0f, 0.0f);	glVertex3f(m_F_x_border, -m_F_y_border, m_F_z_border);
+		glTexCoord2f(1.0f, 1.0f);	glVertex3f(m_F_x_border, m_F_y_border, m_F_z_border);
+		glTexCoord2f(0.0f, 1.0f);	glVertex3f(m_F_x_border, m_F_y_border, -m_F_z_border);
+
+		// Верхняя грань (y = 1.0)
+		//glColor3f(1.0, 0.0, 1.0); // Фиолетовый цвет
+		glTexCoord2f(0.0f, 1.0f);	glVertex3f(-m_F_x_border, m_F_y_border, -m_F_z_border);
+		glTexCoord2f(0.0f, 0.0f);	glVertex3f(m_F_x_border, m_F_y_border, -m_F_z_border);
+		glTexCoord2f(1.0f, 0.0f);	glVertex3f(m_F_x_border, m_F_y_border, m_F_z_border);
+		glTexCoord2f(1.0f, 1.0f);	glVertex3f(-m_F_x_border, m_F_y_border, m_F_z_border);
+
+		// Нижняя грань (y = -1.0)
+		//glColor3f(0.0, 1.0, 1.0); // Голубой цвет
+		glTexCoord2f(1.0f, 1.0f);	glVertex3f(-m_F_x_border, -m_F_y_border, -m_F_z_border);
+		glTexCoord2f(0.0f, 1.0f);	glVertex3f(m_F_x_border, -m_F_y_border, -m_F_z_border);
+		glTexCoord2f(0.0f, 0.0f);	glVertex3f(m_F_x_border, -m_F_y_border, m_F_z_border);
+		glTexCoord2f(1.0f, 0.0f);	glVertex3f(-m_F_x_border, -m_F_y_border, m_F_z_border);
+
+		glEnd();
+		// Конец рисования параллелепипеда
+		// Отключаем текстурирование
+
+		glDisable(GL_TEXTURE_2D);
+		glBindTexture(GL_TEXTURE_2D, 0);
+		glDisable(GL_BLEND);
+
+	}
+
+	void init_scene() {
+		createBalls();
+	}
+
+	int getRand(int leftBorder, int rightBorder) {
+		return (leftBorder + rand() % (rightBorder - leftBorder + 1));
+	}
+
+	bool getCollisionsOfOtherBalls(int index, float x, float y, float z, float radius) {
+
+		for (size_t count = 0; count < m_Balls_balls.getBalls().size(); ++count) {
+			if (count == index)
+				continue;
+			else {
+				float dx = x - m_Balls_balls.getBalls()[count].getCoordsOfCenter().m_F_X;
+				float dy = y - m_Balls_balls.getBalls()[count].getCoordsOfCenter().m_F_Y;
+				float dz = z - m_Balls_balls.getBalls()[count].getCoordsOfCenter().m_F_Z;
+
+				float distance = sqrt(dx * dx + dy * dy + dz * dz);
+
+				if (distance <= m_Balls_balls.getBalls()[count].getRadius() + radius)
+					return false;
+				else return true;
+			}
+		}
+
+	}
+
+	void createBall(float& radius, float& x, float& y, float& z, float& r, float& g, float& b, float& alpha, float& velocityX, float& velocityY, float& velocityZ, bool& isBig) {
+
+		if (isBig) {
+			radius = 1.0f;
+
+			velocityX = 0.0f;
+			velocityY = 0.0f;
+			velocityZ = 0.0f;
+
+			r = 1.0f;
+			g = 0.0f;
+			b = 0.0f;
+			alpha = 1.0f;
+		}
+		else {
+			radius = 0.5f;
+
+			velocityX = 15;
+			velocityY = 15;
+			velocityZ = 15;
+			
+			r = 0.0f;
+			g = 0.0f;
+			b = 0.5f;
+			alpha = 0.3f;
+		}
+
+		x = (float)getRand(-m_F_x_border/2, m_F_x_border/2);
+		y = (float)getRand(-m_F_y_border/2, m_F_y_border/2);
+		z = (float)getRand(-m_F_z_border/2, m_F_z_border/2);
+
+	}
+
+	void createBalls() {
+
+		int countOfSmallBalls = 100;
+		int countOfBigBalls = 1;
+
+		float radius = 0;
+
+		float x = 0;
+		float y = 0;
+		float z = 0;
+
+		float r = 0;
+		float g = 0;
+		float b = 0;
+		float alpha = 1;
+
+		float velocityX = 0;
+		float velocityY = 0;
+		float velocityZ = 0;
+
+		bool isBig = true;
+
+		for (size_t count = 0; count < countOfBigBalls + countOfSmallBalls; ++count) {
+
+			if (count == countOfBigBalls)
+				isBig = false;
+
+			do {
+				createBall(radius, x, y, z, r, g, b, alpha, velocityX, velocityY, velocityZ, isBig);
+
+			} while (getCollisionsOfOtherBalls(count, x, y, z, radius));
+
+			Point3D coords(x, y, z);
+			Color color(r, g, b, alpha);
+			Point3D velocity(velocityX, velocityY, velocityZ);
+
+			m_Balls_balls.setBall(radius, coords, color, velocity);
+		}
+
+		//float r = ((float)rand()) / RAND_MAX;
+		//float g = ((float)rand()) / RAND_MAX;
+		//float b = ((float)rand()) / RAND_MAX;
+
+	}
+};
---
+++ MMDTT_LAB_2/source/MMDTT_LAB_2.cpp
@@ -0,0 +1,244 @@
+#include <iostream>
+#include <vector>
+
+class LAB2 {
+
+private:
+
+	/*
+		Начальный момент:
+			1) скорости
+			2) координаты
+	
+	*/
+
+	int m_I_n; // Количество узлов сетки между граничными узлами подобласти
+	int m_I_k;
+
+	double m_D_left_border = 0; // Координаты границ подобластей 
+	double m_D_right_border = 0; // Координаты границ подобластей
+
+	int m_I_ЕР = 0; // Параметр физической постановки задачи
+
+	int m_I_GRU = 0; // Тип граничных условий (0 – отсутствует перемещение вдоль оси; 
+	// 1 – свободное перемещение вдоль оси; 2 – перемещение со скоростью v(t) вдоль оси;
+	// 3 – перемещение вдоль оси под действием поверхностной нагрузки P(t)
+
+	double m_D_K = 0; // Модуль объёмного сжатия (только при EP=0)
+	double m_D_G = 0; // Модуль сдвига (только при EP=0)
+	double m_D_RO = 0; // Плотность (только при EP=0)
+
+	double m_D_KB = 0; // Коэффициент запаса к временному шагу
+
+	int m_I_N_velocity; // Размерность таблицы v(t) (только при GRU=2)
+	std::vector<double> m_Vec_T_velocity; // Таблица v(t) : моменты времени (только при GRU=2)
+	std::vector<double> m_Vec_F_velocity; // Таблица v(t) : значения функции (только при GRU=2)
+
+	int m_I_N_power; // Размерность таблицы P(t) (только GRU = 3)
+	std::vector<double> m_Vec_T_power; // Таблица P(t) : моменты времени (только при GRU = 3)
+	std::vector<double> m_Vec_F_power; // Таблица P(t) : значения функции (только при GRU = 3)
+
+	int m_I_POL = 0; // Тип начальных полей (0 – начальные поля нулевые; 
+	// 1 – начальные поля постоянны; 2 – нестандартные начальные поля)
+
+	int m_I_KP; // Количество задаваемых начальных полей (только при POL = 1)
+	std::vector<int> m_Vec_NP; // Номера задаваемых начальных полей:
+	// 1 – поле скоростей; 2 – поле перемещений; 3 – поле напряжений; 4 – поле деформаций
+
+	std::vector<double> m_Vec_B; // Значения задаваемых полей
+
+	////////////////////////////
+
+	std::vector<std::vector<double>> m_2Vec_Velocity_of_moveming; // скорость перемещений
+	std::vector<std::vector<double>> m_2Vec_Delta_of_moveming; // приращение перемещений
+	std::vector<std::vector<double>> m_2Vec_Geometry; // геометрия
+
+	std::vector<std::vector<double>> m_2Vec_Velocity_of_deformation; // скорость деформации
+	std::vector<std::vector<double>> m_2Vec_Delta_of_deformation; // приращение деформации
+	std::vector<std::vector<double>> m_2Vec_Deformation; // деформации
+
+	std::vector<std::vector<double>> m_2Vec_Delta_deformation_of_sphere; // приращение шара деформации
+	std::vector<std::vector<double>> m_2Vec_Delta_sigma_of_sphere; // приращение шара напряжений
+	std::vector<std::vector<double>> m_2Vec_Sphere_of_sigma; // шар напряжения
+	
+	std::vector<std::vector<double>> m_2Vec_Delta_deviator_of_deformation; // приращение девиатора деформации
+	std::vector<std::vector<double>> m_2Vec_Deviator_of_sigma; // девиатор напряжений
+	//std::vector<std::vector<double>> m_2Vec_Sphere_of_sigma; // шар напряжения
+
+	std::vector<std::vector<double>> m_2Vec_Sigma; // напряжения
+	std::vector<std::vector<double>> m_2Vec_Vertex_power; // узловые силы
+	double m_D_Delta_Time; // шаг по времени
+
+public:
+
+	LAB2() {
+	}
+
+	void test_first(const double& left_border, const double& right_border, int count_of_div_coords,
+		int count_of_time_moments, const double& start_time_moment, const double& end_time_mometn) { // левый конец жестко закреплен, правый конец двигается
+
+		double step_coords = abs(left_border - right_border) / (2 * count_of_div_coords);
+		double step_time_moments = abs(end_time_mometn - start_time_moment) / (count_of_time_moments);
+
+		m_D_Delta_Time = step_time_moments;
+
+		m_I_n = 2 * count_of_div_coords + 1;
+		m_I_k = 2 * count_of_time_moments + 1;
+
+		m_2Vec_Velocity_of_moveming.clear();
+		m_2Vec_Delta_of_moveming.clear();
+		m_2Vec_Geometry.clear();
+		m_2Vec_Velocity_of_deformation.clear();
+		m_2Vec_Delta_of_deformation.clear();
+		m_2Vec_Deformation.clear();
+		m_2Vec_Delta_deformation_of_sphere.clear();
+		m_2Vec_Delta_sigma_of_sphere.clear();
+		m_2Vec_Sphere_of_sigma.clear();
+		m_2Vec_Delta_deviator_of_deformation.clear();
+		m_2Vec_Deviator_of_sigma.clear();
+		m_2Vec_Sigma.clear();
+		m_2Vec_Vertex_power.clear();
+
+		m_2Vec_Geometry.resize(m_I_n);
+		m_2Vec_Delta_of_moveming.resize(m_I_n);
+		m_2Vec_Velocity_of_moveming.resize(m_I_n);
+		m_2Vec_Velocity_of_deformation.resize(m_I_n);
+		m_2Vec_Delta_of_deformation.resize(m_I_n);
+		m_2Vec_Deformation.resize(m_I_n);
+		m_2Vec_Delta_deformation_of_sphere.resize(m_I_n);
+		m_2Vec_Delta_sigma_of_sphere.resize(m_I_n);
+		m_2Vec_Sphere_of_sigma.resize(m_I_n);
+		m_2Vec_Delta_deviator_of_deformation.resize(m_I_n);
+		m_2Vec_Deviator_of_sigma.resize(m_I_n);
+		m_2Vec_Sigma.resize(m_I_n);
+		m_2Vec_Vertex_power.resize(m_I_n);
+
+		for (size_t i = 0; i < m_I_n; ++i) {
+			m_2Vec_Geometry[i].resize(m_I_k, 0);
+			m_2Vec_Delta_of_moveming[i].resize(m_I_k, 0);
+			m_2Vec_Velocity_of_moveming[i].resize(m_I_k, 0);
+			m_2Vec_Velocity_of_deformation[i].resize(m_I_k, 0);
+			m_2Vec_Delta_of_deformation[i].resize(m_I_k, 0);
+			m_2Vec_Deformation[i].resize(m_I_k, 0);
+			m_2Vec_Delta_deformation_of_sphere[i].resize(m_I_k, 0);
+			m_2Vec_Delta_sigma_of_sphere[i].resize(m_I_k, 0);
+			m_2Vec_Sphere_of_sigma[i].resize(m_I_k, 0);
+			m_2Vec_Delta_deviator_of_deformation[i].resize(m_I_k, 0);
+			m_2Vec_Deviator_of_sigma[i].resize(m_I_k, 0);
+			m_2Vec_Sigma[i].resize(m_I_k, 0);
+			m_2Vec_Vertex_power[i].resize(m_I_k, 0);
+		}
+
+		for (size_t j = 0; j < m_I_k; ++j) {
+			m_2Vec_Velocity_of_moveming[m_I_n - 1][j] = 1;
+		}
+
+		for (size_t i = 0; i < m_I_n; ++i) { // по столбцам
+			m_2Vec_Geometry[i][0] = left_border + i * step_coords;
+		}
+	}
+
+	void print_Geometry() {
+
+		std::cout << "m_2Vec_Geometry: size(" << m_I_n << " - stolb; " << m_I_k << " - stroki)" << std::endl;
+
+		for (int j = m_I_k - 1; j >= 0; --j) {
+			for (int i = 0; i < m_I_n; ++i) {
+				std::cout << m_2Vec_Geometry[i][j] << "\t";
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	void print_Velocity_of_moveming() {
+
+		std::cout << "print_Velocity_of_moveming: size(" << m_I_n << " - stolb; " << m_I_k << " - stroki)" << std::endl;
+
+		for (int j = m_I_k - 1; j >= 0; --j) {
+			for (int i = 0; i < m_I_n; ++i) {
+				std::cout << m_2Vec_Velocity_of_moveming[i][j] << "\t";
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	void print_Delta_of_moveming() {
+
+		std::cout << "print_Vec_Delta_of_moveming: size(" << m_I_n << " - stolb; " << m_I_k << " - stroki)" << std::endl;
+
+		for (int j = m_I_k - 1; j >= 0; --j) {
+			for (int i = 0; i < m_I_n; ++i) {
+				std::cout << m_2Vec_Delta_of_moveming[i][j] << "\t";
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	void print_Velocity_of_deformation() {
+		std::cout << "print_Velocity_of_deformation: size(" << m_I_n << " - stolb; " << m_I_k << " - stroki)" << std::endl;
+
+		for (int j = m_I_k - 1; j >= 0; --j) {
+			for (int i = 0; i < m_I_n; ++i) {
+				std::cout << m_2Vec_Velocity_of_deformation[i][j] << "\t";
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	void calc_delta_of_moveming() {
+
+		for (int j = 0; j < m_I_k - 1; j+=2) {
+			for (int i = 0; i < m_I_n; i+=2) {
+				
+				m_2Vec_Delta_of_moveming[i][j + 2] = m_2Vec_Delta_of_moveming[i][j] + m_D_Delta_Time *
+					m_2Vec_Velocity_of_moveming[i][j + 1];
+
+				m_2Vec_Delta_of_moveming[i][j + 1] = m_2Vec_Delta_of_moveming[i][j] + (double)1 / 2 *
+					m_D_Delta_Time * m_2Vec_Velocity_of_moveming[i][j + 1];
+
+			}
+		}
+	}
+
+	void calc_geometry() {
+
+		for (int j = 0; j < m_I_k - 1; j += 2) {
+			for (int i = 0; i < m_I_n; ++i) {
+				m_2Vec_Geometry[i][j + 1] = m_2Vec_Geometry[i][0] + m_2Vec_Delta_of_moveming[i][j + 1];
+				m_2Vec_Geometry[i][j + 2] = m_2Vec_Geometry[i][0] + m_2Vec_Delta_of_moveming[i][j + 2];
+			}
+		}
+	}
+
+	void calc_velocity_of_deformation() {
+
+		for (int j = 1; j < m_I_k - 1; j+=2) {
+			for (int i = 1; i < m_I_n - 1; i+=2) {
+
+				m_2Vec_Velocity_of_deformation[i][j] = (m_2Vec_Delta_of_moveming[i + 1][j] - m_2Vec_Delta_of_moveming[i-1][j]) /
+					(m_2Vec_Geometry[i + 1][j] - m_2Vec_Geometry[i-1][j]);
+
+			}
+		}
+	}
+
+};
+
+int main() {
+
+	LAB2 lab2;
+
+	lab2.test_first(0, 1, 2, 2, 0, 5);
+
+	lab2.print_Geometry();
+	lab2.print_Velocity_of_moveming();
+
+	lab2.calc_delta_of_moveming();
+	lab2.print_Delta_of_moveming();
+	lab2.calc_geometry();
+	lab2.print_Geometry();
+	lab2.calc_velocity_of_deformation();
+	lab2.print_Velocity_of_deformation();
+
+	return 0;
+}
