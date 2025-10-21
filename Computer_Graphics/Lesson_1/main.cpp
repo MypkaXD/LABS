49099c6b9cccad5282e84ad3488907c2b0fb4f37
MypkaXD <nicklayschannel@yandex.ru>
2025-10-21 11:51:18 +0300

    first lab_1 computer_graphics

--- Computer_Graphics/Lesson_1/main.cpp
+++ Computer_Graphics/Lesson_1/main.cpp
@@ -8,26 +8,32 @@
 #include "vtkSmartPointer.h"
 #include "vtkStructuredPoints.h"
 #include "vtkAutoInit.h"
+
 VTK_MODULE_INIT(vtkRenderingOpenGL2)
 VTK_MODULE_INIT(vtkInteractionStyle)
 
 int main()
 {
+	srand(0);
+
 	vtkSmartPointer<vtkStructuredPoints> SP =
 		vtkSmartPointer<vtkStructuredPoints>::New();
-	SP->SetOrigin(0, 0, 0);
-	SP->SetDimensions(3, 4, 5);
-	SP->SetSpacing(1.33, 1.25, 1);
+	SP->SetOrigin(0, 0, 0); // Начало координат
+	SP->SetDimensions(10, 10, 10); // Размер сетки
+	SP->SetSpacing(10, 10, 10); // Размер между узлами сетки - x,y,z
 
 	vtkSmartPointer<vtkFloatArray> FA =
 		vtkSmartPointer<vtkFloatArray>::New();
-	for (int i = 0; i < 3 * 4 * 5; i++)
-		FA->InsertValue(i, i * 0.02);
-	SP->GetPointData()->SetScalars(FA);
+	for (int i = 0; i < 10 * 10 * 10; i++) {
+		FA->InsertValue(i, i * 0.02); // Заполняем массив значениями
+		//FA->InsertValue(i, (float)rand() / RAND_MAX); // Заполняем массив значениями
+	}
+	SP->GetPointData()->SetScalars(FA); // Привязка данных к сетке
 
 	vtkSmartPointer<vtkDataSetMapper> mapper =
 		vtkSmartPointer<vtkDataSetMapper>::New();
 	mapper->SetInputData(SP);
+	mapper->SetScalarRange(0, 20); // диапазон значений для выбора цвета, 0 - красный, 20 - синий
 
 	vtkSmartPointer<vtkActor> actor =
 		vtkSmartPointer<vtkActor>::New();
